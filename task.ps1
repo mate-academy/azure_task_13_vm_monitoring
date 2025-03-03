1cd8128918d20d87d557590db8544e0e88067eeb
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-03-03 20:29:36 +0100

    solution

--- task.ps1
+++ task.ps1
@@ -6,12 +6,12 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabel = "matetask" + (Get-Random -Count 1)
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -42,7 +42,9 @@ New-AzVm `
 -SubnetName $subnetName `
 -VirtualNetworkName $virtualNetworkName `
 -SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
+-SshKeyName $sshKeyName `
+-PublicIpAddressName $publicIpAddressName `
+-SystemAssignedIdentity
 
 Write-Host "Installing the TODO web app..."
 $Params = @{
@@ -56,4 +58,18 @@ $Params = @{
 }
 Set-AzVMExtension @Params
 
-# Install Azure Monitor Agent VM extention -> 
+# Install Azure Monitor Agent VM extention ->
+Write-Host "Installing the Azure Monitor Agent..."
+$Params = @{
+    ResourceGroupName      = $resourceGroupName
+    VMName                 = $vmName
+    Name                   = 'AzureMonitorLinuxAgent'
+    Publisher              = 'Microsoft.Azure.Monitor'
+    ExtensionType          = 'AzureMonitorLinuxAgent'
+    TypeHandlerVersion     = '1.9'
+    Location               = $location
+    EnableAutomaticUpgrade = $true
+}
+Set-AzVMExtension @Params
+
+
