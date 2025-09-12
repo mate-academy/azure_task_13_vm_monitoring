e7b562499ad4b1c5bcf2c0865c35ced4d374524e
litvi <litvin4uk.roman@gmail.com>
2025-09-13 00:06:43 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -6,12 +6,12 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabel = "matetask" + (Get-Random -Count 1)
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -29,7 +29,7 @@ Write-Host "Creating a SSH key ..."
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
 Write-Host "Creating a Public IP Address ..."
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
+New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
 
 Write-Host "Creating a VM ..."
 # Update the VM deployment command to enable a system-assigned mannaged identity on it. 
@@ -42,7 +42,8 @@ New-AzVm `
 -SubnetName $subnetName `
 -VirtualNetworkName $virtualNetworkName `
 -SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
+-SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName `
+-SystemAssignedIdentity
 
 Write-Host "Installing the TODO web app..."
 $Params = @{
@@ -56,4 +57,29 @@ $Params = @{
 }
 Set-AzVMExtension @Params
 
-# Install Azure Monitor Agent VM extention -> 
+# Install Azure Monitor Agent VM extention ->
+Set-AzVMExtension `
+-Name AzureMonitorLinuxAgent `
+-ExtensionType AzureMonitorLinuxAgent `
+-Publisher Microsoft.Azure.Monitor `
+-ResourceGroupName $resourceGroupName `
+-VMName $vmName `
+-Location $location `
+-TypeHandlerVersion "1.36" `
+-EnableAutomaticUpgrade $true
+
+$prefix = "mystorage"
+$suffix = (New-Guid).Guid.Substring(0,6).ToLower()
+$storageAccountName = $prefix + $suffix
+
+
+$StorageHT = @{
+  ResourceGroupName = $resourceGroupName
+  Name              = $storageAccountName
+  SkuName           = 'Standard_LRS'
+  Location          =  $location
+}
+$StorageAccount = New-AzStorageAccount @StorageHT
+$Context = $StorageAccount.Context
+$ContainerName = 'task-artifacts'
+New-AzStorageContainer -Name $ContainerName -Context $Context
