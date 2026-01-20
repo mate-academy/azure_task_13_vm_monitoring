a4f86dd88aa644366fe3d0c11dcbac5107b0becb
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-20 16:01:53 +0200

    Refactor start.sh and install-app.sh for improved setup; add artifacts.json and update task.ps1 for Azure configurations

--- app/start.sh
+++ app/start.sh
@@ -1,18 +1,17 @@
-#! /bin/bash 
+#!/bin/bash
 
-echo "Preparing the system for a check..."
-# Create a symlinc to the folder with VM extention logs, so we can
-# validate that azure monitor agent is sending metrics by checking 
-# the Azure Monitor Agend log /var/opt/microsoft/azuremonitoragent/log/mdsd.info
-ln -s  /var/opt/microsoft /app/todolist/static/files
+mkdir -p /app/todolist/static/files
 
-lsblk -o NAME,HCTL,SIZE,MOUNTPOINT > /data/app/todolist/static/files/task3.log
+ln -sf /var/opt/microsoft /app/todolist/static/files/
+
+lsblk -o NAME,HCTL,SIZE,MOUNTPOINT > /app/todolist/static/files/task3.log
 
 cd /app
-
 python3 -m venv venv
 source venv/bin/activate
 
+pip install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py migrate
+
 python3 manage.py runserver 0.0.0.0:8080
--- install-app.sh
+++ install-app.sh
@@ -4,22 +4,24 @@ apt-get update -yq
 apt-get install python3-pip python3-venv git -yq
 
 rm -rf /app
-mkdir /app
+mkdir -p /app
 
 cd /tmp
-rm -rf azure_task_12_deploy_app_with_vm_extention
-git clone https://github.com/mate-academy/azure_task_13_vm_monitoring.git
+rm -rf azure_task_13_vm_monitoring
+git clone https://github.com/whatislavx/azure_task_13_vm_monitoring.git
 
-find /tmp/azure_task_12_deploy_app_with_vm_extention/app -type f -exec sed -i 's/\r$//' {} +
+find /tmp/azure_task_13_vm_monitoring/app -type f -exec sed -i 's/\r$//' {} +
 
-cp -r /tmp/azure_task_12_deploy_app_with_vm_extention/app/* /app/
+cp -r /tmp/azure_task_13_vm_monitoring/app/* /app/
 
 mv /app/todoapp.service /etc/systemd/system/
-
 chmod +x /app/start.sh
 
-systemctl daemon-reload
-systemctl start todoapp
-systemctl enable todoapp
+chown -R id010806:id010806 /app
+chmod -R 755 /app
 
-rm -rf /tmp/azure_task_12_deploy_app_with_vm_extention
+systemctl daemon-reload
+systemctl enable todoapp
+systemctl start todoapp
+
+rm -rf /tmp/azure_task_13_vm_monitoring
