5c45e3a45809ac9d06ecc7c43b9b62b592d07bca
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-01-20 14:06:02 +0200

    Enhance install-app.sh for app setup and dependencies
    Updated installation script to include python3-venv and git. Adjusted app directory handling and fixed file paths for cloning and copying.

--- install-app.sh
+++ install-app.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Script to silently install and start the todo web app on the virtual machine. 
-# Note that all commands bellow are without sudo - that's because extention mechanism 
-# runs scripts under root user. 
-
-# install system updates and isntall python3-pip package using apt. '-yq' flags are 
-# used to suppress any interactive prompts - we won't be able to confirm operation 
-# when running the script as VM extention.  
 apt-get update -yq
-apt-get install python3-pip -yq
+apt-get install python3-pip python3-venv git -yq
 
-# Create a directory for the app and download the files. 
-mkdir /app 
-# make sure to uncomment the line bellow and update the link with your GitHub username
+rm -rf /app
+mkdir /app
+
+cd /tmp
+rm -rf azure_task_12_deploy_app_with_vm_extention
 git clone https://github.com/mate-academy/azure_task_13_vm_monitoring.git
-cp -r azure_task_13_vm_monitoring/app/* /app
 
-# create a service for the app via systemctl and start the app
+find /tmp/azure_task_12_deploy_app_with_vm_extention/app -type f -exec sed -i 's/\r$//' {} +
+
+cp -r /tmp/azure_task_12_deploy_app_with_vm_extention/app/* /app/
+
 mv /app/todoapp.service /etc/systemd/system/
+
+chmod +x /app/start.sh
+
 systemctl daemon-reload
 systemctl start todoapp
 systemctl enable todoapp
+
+rm -rf /tmp/azure_task_12_deploy_app_with_vm_extention
