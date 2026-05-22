589a2b16c89fa19f7a897a2c8f4e1a1ac88eaa36
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-05-23 00:19:56 +0300

    aded data rulles

--- task.ps1
+++ task.ps1
@@ -1,4 +1,5 @@
-$location = "uksouth"
+$vmName = "monitoringVM"
+$location = "uaenorth"
 $resourceGroupName = "mate-azure-task-13"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,11 +7,11 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "$HOME\.ssh\id_rsa.pub" 
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$vmSize = "Standard_D2s_v3"
 $dnsLabel = "matetask" + (Get-Random -Count 1) 
 
 Write-Host "Creating a resource group $resourceGroupName ..."
@@ -29,20 +30,33 @@ Write-Host "Creating a SSH key ..."
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
 Write-Host "Creating a Public IP Address ..."
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
+New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
 
 Write-Host "Creating a VM ..."
 # Update the VM deployment command to enable a system-assigned mannaged identity on it. 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
+## User-assigned managed identity
+
+
+New-AzVm -ResourceGroupName $resourceGroupName `
+    -Name $vmName -Location $location `
+    -Image $vmImage -Size $vmSize `
+    -SubnetName $subnetName -VirtualNetworkName $virtualNetworkName `
+    -SecurityGroupName $networkSecurityGroupName `
+    -SshKeyName $sshKeyName -PublicIpAddressName $publicIpAddressName `
+
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
+Update-AzVM -ResourceGroupName $resourceGroupName -VM $vm -IdentityType SystemAssigned
+
+Set-AzVMExtension -Name AzureMonitorLinuxAgent `
+    -ExtensionType AzureMonitorLinuxAgent `
+    -Publisher Microsoft.Azure.Monitor `
+    -ResourceGroupName $resourceGroupName `
+    -VMName $vmName `
+    -Location $location `
+    -TypeHandlerVersion 1.0 `
+    -EnableAutomaticUpgrade $true
+
+
 
 Write-Host "Installing the TODO web app..."
 $Params = @{
@@ -52,8 +66,10 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_13_vm_monitoring/main/install-app.sh'); commandToExecute = './install-app.sh'}
+    Settings          = @{fileUris = @('https://raw.githubusercontent.com/Xandane/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh'); commandToExecute = './install-app.sh'}
 }
 Set-AzVMExtension @Params
+
+
 
 # Install Azure Monitor Agent VM extention -> 
