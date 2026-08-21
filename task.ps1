522cae0ab8280fa235bb580e8675a269d6855668
Vitalii <stalchenkovitaliy@gmail.com>
2026-08-21 13:23:36 +0400

    Configure VM monitoring

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "polandcentral"
 $resourceGroupName = "mate-azure-task-13"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,54 +6,122 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$vmSize = "Standard_B2ats_v2"
+$dnsLabel = "matetask" + (Get-Random -Count 1)
 
 Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+New-AzResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location
 
 Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
-New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig `
+    -Name SSH `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 1001 `
+    -SourceAddressPrefix * `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 22 `
+    -Access Allow
+
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig `
+    -Name HTTP `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 1002 `
+    -SourceAddressPrefix * `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 8080 `
+    -Access Allow
+
+New-AzNetworkSecurityGroup `
+    -Name $networkSecurityGroupName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
 Write-Host "Creating a virtual network ..."
-$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+
+$subnet = New-AzVirtualNetworkSubnetConfig `
+    -Name $subnetName `
+    -AddressPrefix $subnetAddressPrefix
+
+New-AzVirtualNetwork `
+    -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $subnet
 
 Write-Host "Creating a SSH key ..."
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+
+New-AzSshKey `
+    -Name $sshKeyName `
+    -ResourceGroupName $resourceGroupName `
+    -PublicKey $sshKeyPublicKey
 
 Write-Host "Creating a Public IP Address ..."
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
+
+New-AzPublicIpAddress `
+    -Name $publicIpAddressName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -Sku Standard `
+    -AllocationMethod Static `
+    -DomainNameLabel $dnsLabel
 
 Write-Host "Creating a VM ..."
-# Update the VM deployment command to enable a system-assigned mannaged identity on it. 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
 
-Write-Host "Installing the TODO web app..."
+New-AzVm `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vmName `
+    -Location $location `
+    -Image $vmImage `
+    -Size $vmSize `
+    -SubnetName $subnetName `
+    -VirtualNetworkName $virtualNetworkName `
+    -SecurityGroupName $networkSecurityGroupName `
+    -SshKeyName $sshKeyName `
+    -PublicIpAddressName $publicIpAddressName `
+    -SystemAssignedIdentity
+
+Write-Host "Installing the TODO web app ..."
+
 $Params = @{
     ResourceGroupName  = $resourceGroupName
     VMName             = $vmName
-    Name               = 'CustomScript'
-    Publisher          = 'Microsoft.Azure.Extensions'
-    ExtensionType      = 'CustomScript'
-    TypeHandlerVersion = '2.1'
-    Settings          = @{fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_13_vm_monitoring/main/install-app.sh'); commandToExecute = './install-app.sh'}
+    Name               = "CustomScript"
+    Publisher          = "Microsoft.Azure.Extensions"
+    ExtensionType      = "CustomScript"
+    TypeHandlerVersion = "2.1"
+
+    Settings = @{
+        fileUris = @(
+            "https://raw.githubusercontent.com/Loki-sudo007/azure_task_13_vm_monitoring/main/install-app.sh"
+        )
+
+        commandToExecute = "bash install-app.sh"
+    }
 }
+
 Set-AzVMExtension @Params
 
-# Install Azure Monitor Agent VM extention -> 
+Write-Host "Installing Azure Monitor Agent ..."
+
+Set-AzVMExtension `
+    -Name "AzureMonitorLinuxAgent" `
+    -ExtensionType "AzureMonitorLinuxAgent" `
+    -Publisher "Microsoft.Azure.Monitor" `
+    -ResourceGroupName $resourceGroupName `
+    -VMName $vmName `
+    -Location $location `
+    -TypeHandlerVersion "1.0" `
+    -EnableAutomaticUpgrade $true
